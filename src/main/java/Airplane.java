975413515bd703d7162d7d518e47458706ff2871
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-10-01 22:38:30 +0200

    Airplane Class created

--- src/main/java/Airplane.java
+++ src/main/java/Airplane.java
@@ -1,2 +1,66 @@
+import java.util.Map;
+
 public class Airplane {
+
+    private Map<String, Integer> destination;
+    private boolean isBusinessClass;
+    private Integer[][] seatMap;
+    private double price, luggageAllowance, extraLuggageFeePerKilo;
+
+    public Airplane() {
+    }
+    public Airplane(Map<String, Integer> destination, boolean isBusinessClass, double price, double luggageAllowance) {
+        this.destination = destination;
+        this.isBusinessClass = isBusinessClass;
+        this.price = price;
+        this.luggageAllowance = luggageAllowance;
+    }
+
+    public Map<String, Integer> getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Map<String, Integer> destination) {
+        this.destination = destination;
+    }
+
+    public boolean isBusinessClass() {
+        return isBusinessClass;
+    }
+
+    public void setBusinessClass(boolean businessClass) {
+        isBusinessClass = businessClass;
+    }
+
+    public Integer[][] getSeatMap() {
+        return seatMap;
+    }
+
+    public void setSeatMap(Integer[][] seatMap) {
+        this.seatMap = seatMap;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getLuggageAllowance() {
+        return luggageAllowance;
+    }
+
+    public void setLuggageAllowance(double luggageAllowance) {
+        this.luggageAllowance = luggageAllowance;
+    }
+
+    public double getExtraLuggageFeePerKilo() {
+        return extraLuggageFeePerKilo;
+    }
+
+    public void setExtraLuggageFeePerKilo(double extraLuggageFeePerKilo) {
+        this.extraLuggageFeePerKilo = extraLuggageFeePerKilo;
+    }
 }
